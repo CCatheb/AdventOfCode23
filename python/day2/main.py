bfdd47dd1728282f6f5968b187943479d567f07e
Cédric C <cathebras-cedric@orange.fr>
2023-12-03 17:14:31 +0100

    Python Day 2: Add some comments, remove debug prints

--- python/day2/main.py
+++ python/day2/main.py
@@ -15,7 +15,8 @@ def read_file(path: str) -> list :
         return f.readlines()
 
 class game() :
-    
+    """ This class defines a game and the basics elements needed to do the job. 
+    """
     def __init__(self) -> None:
         self.id = 0
         self.data = ""
@@ -38,25 +39,31 @@ class game() :
 
     @staticmethod
     def find_number(string: str) -> int:
+        """ This function returns the first digital value found in the passed string """
         return int(re.findall('[0-9]+', string)[0])
 
     def set_game_id(self) -> int:
+        """ This function sets the game identifier """
         self.id = self.find_number(self.data.split(':')[0])
 
     def find_rolls(self) -> list:
+        """ This function set the list of rolls made in the current game using the ; separator """
         self.rolls = self.data.split(':')[1].split(';')
 
     def find_color_in_roll(self, color: str, rolls: list) -> int:
+        """ This function is used to fin the specified color in the specified roll, and returns the number of cubes
+        that have this color.
+        """
         rolls = rolls.split(',')
         for j in range(len(rolls)):
             roll = rolls[j]
             if color in roll:
-                #print(f"Found {color} in '{roll[i]}'\n\tVALUE = {self.find_number(roll[i])}")
                 return self.find_number(roll)
         return 0
 
 if __name__ ==  "__main__":
 
+    # Setup all the needed vars
     list_of_games = []
     list_of_valid_games = []
     colors = {
@@ -64,42 +71,51 @@ if __name__ ==  "__main__":
         'green': 13,
         'blue': 14,
     }
+    
+    # Read the input file
     file_data = read_file("dataset.txt")
 
     total_id = 0
     total_power = 0
 
+    # Parse the data and set all the games
     for data in file_data:
         my_game = game()
         list_of_games.append(my_game)
         my_game.data = data
         my_game.set_game_id()
         my_game.find_rolls()
+
+        # Start of real "logic" part here
+        # Iterate through all the rolls
         for roll in my_game.rolls:
-            #print(roll)
+            # For each color, get the value of dices in the current roll
             for color, max_val in colors.items():
-                #print(f"{color}\t{max_val}")
                 value = my_game.find_color_in_roll(color, roll)
-                #print(f"GAME {my_game.id}\n\tROLL\t{roll}\n\tCOLOR\t{color}\n\t\tFOUND {value} MAX IS {max_val}")
+
+                # If we are over the max value set the color as unvalid
                 if value > max_val:
                     my_game.colors[color]["status"] = False
+                # This next part is about finding the max amout of dices needed
                 if value > my_game.colors[color]["count"]:
                     my_game.colors[color]["count"] = value
+
+        # Calculate the power of the game
         for color in my_game.colors.values():
             print(f"Game {my_game.id} got power of {my_game.power}")
             my_game.power *= color["count"]
 
         list_of_valid_games.append(my_game)
 
+    # Parse all the valid games, and add their total ID
     for my_game in list_of_valid_games:
         if all(color["status"] for color in my_game.colors.values()):
-            print(f"Game {my_game.id} is valid.")
             total_id = total_id + my_game.id
 
+    # For all the games, add their total power
     for my_game in list_of_games:
         total_power = total_power + my_game.power
 
-        
-
+    # Print the result
     print(total_id)
     print(total_power)
